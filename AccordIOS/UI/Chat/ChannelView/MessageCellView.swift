10e0a10272e448d7db5b2d58a24e580e487f6d68
Superbro <65824876+Superbro9@users.noreply.github.com>
2022-05-18 17:15:32 +0100

    fix: make typing smooth again
    Markdown stuff not added
    
    https://github.com/evelyneee/accord/commit/7771574366887ee9c34d8774601d069a362b828f

--- AccordIOS/UI/Chat/ChannelView/MessageCellView.swift
+++ AccordIOS/UI/Chat/ChannelView/MessageCellView.swift
@@ -204,7 +204,9 @@ struct MessageCellView: View, Equatable {
                  self.editing.toggle()
             }.disabled(message.author?.id != AccordCoreVars.user?.id)
             Button("Delete") { [weak message] in
-                message?.delete()
+                DispatchQueue.global().async {
+                    message?.delete()
+                }
             }.disabled(message.author?.id != AccordCoreVars.user?.id)
             Divider()
             Button("Show profile") {
