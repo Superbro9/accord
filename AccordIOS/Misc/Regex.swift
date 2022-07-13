65ccf9909548e8bd5c2bf4a3876e59956b129c3a
Superbro <65824876+Superbro9@users.noreply.github.com>
2022-07-13 15:17:57 +0100

    fix: iconless server in folder view
    https://github.com/evelyneee/accord/commit/3a79f2fbfa56851c4fcd62ed24384b027ed9195d
    
    Added some other stuff

--- AccordIOS/Misc/Regex.swift
+++ AccordIOS/Misc/Regex.swift
@@ -1,5 +1,5 @@
 //
-//  RegexExpressions.swift.swift
+//  RegexExpressions.swift
 //  Accord
 //
 //  Created by evelyn on 2022-03-07.
@@ -8,11 +8,9 @@
 import Foundation
 
 enum RegexExpressions {
-    
     static func precompute() {
         _ = (
             fullEmojiRegex,
-            lineSplitRegex,
             emojiIDRegex,
             songIDsRegex,
             mentionsRegex,
@@ -26,18 +24,32 @@ enum RegexExpressions {
             completedEmoteRegex
         )
     }
-    
-    static var fullEmojiRegex = try? NSRegularExpression(pattern: #"<:\w+:[0-9]+>"#)
-    static var lineSplitRegex = try? NSRegularExpression(pattern: #"\*.+\*|~~.+~~|`{1,3}.+`{1,3}|([^*~\s]+)+"#)
-    static var emojiIDRegex = try? NSRegularExpression(pattern: #"(?<=\:)(\d+)(.*?)(?=\>)"#)
-    static var songIDsRegex = try? NSRegularExpression(pattern: #"(?<=https:\/\/open\.spotify\.com\/track\/|https:\/\/music\.apple\.com\/[a-z][a-z]\/album\/[a-zA-Z\d%\(\)-]{1,100}\/|https://tidal\.com/browse/track/)(?:(?!\?).)*"#)
-    static var mentionsRegex = try? NSRegularExpression(pattern: #"(?<=\@|@!)(\d+)(.*?)(?=\>)"#)
-    static var platformsRegex = try? NSRegularExpression(pattern: #"(spotify|music\.apple|tidal)"#)
-    static var channelsRegex = try? NSRegularExpression(pattern: ##"(?<=\#)(\d+)(.+?)(?=\>)"##)
-    static var lineRegex = try? NSRegularExpression(pattern: #"\*.+\*|~~.+~~|`{1,3}.+`{1,3}|([^*~\s]+)+"#)
-    static var chatTextMentionsRegex = try? NSRegularExpression(pattern: #"(?<=@)(?:(?!\ ).)*"#)
-    static var chatTextChannelsRegex = try? NSRegularExpression(pattern: #"(?<=#)(?:(?!\ ).)*"#)
-    static var chatTextSlashCommandRegex = try? NSRegularExpression(pattern: #"(?<=\/)(?:(?!\ ).)*"#)
-    static var chatTextEmojiRegex = try? NSRegularExpression(pattern: #"(?<=:).*"#)
-    static var completedEmoteRegex = try? NSRegularExpression(pattern: "(?<!<|<a):.+:")
+
+    static var fullEmojiRegex = try? NSRegularExpression(pattern: RegexLiterals.fullEmojiRegex.rawValue)
+    static var emojiIDRegex = try? NSRegularExpression(pattern: RegexLiterals.emojiIDRegex.rawValue)
+    static var songIDsRegex = try? NSRegularExpression(pattern: RegexLiterals.songIDsRegex.rawValue)
+    static var mentionsRegex = try? NSRegularExpression(pattern: RegexLiterals.mentionsRegex.rawValue)
+    static var platformsRegex = try? NSRegularExpression(pattern: RegexLiterals.platformsRegex.rawValue)
+    static var channelsRegex = try? NSRegularExpression(pattern: RegexLiterals.channelsRegex.rawValue)
+    static var lineRegex = try? NSRegularExpression(pattern: RegexLiterals.lineRegex.rawValue)
+    static var chatTextMentionsRegex = try? NSRegularExpression(pattern: RegexLiterals.chatTextMentionsRegex.rawValue)
+    static var chatTextChannelsRegex = try? NSRegularExpression(pattern: RegexLiterals.chatTextChannelsRegex.rawValue)
+    static var chatTextSlashCommandRegex = try? NSRegularExpression(pattern: RegexLiterals.chatTextSlashCommandRegex.rawValue)
+    static var chatTextEmojiRegex = try? NSRegularExpression(pattern: RegexLiterals.chatTextEmojiRegex.rawValue)
+    static var completedEmoteRegex = try? NSRegularExpression(pattern: RegexLiterals.completedEmoteRegex.rawValue)
+}
+
+enum RegexLiterals: String, RawRepresentable {
+    case fullEmojiRegex = #"<:\w+:[0-9]+>"#
+    case emojiIDRegex = #"(?<=\:)(\d+)(.*?)(?=\>)"#
+    case songIDsRegex = #"(?<=https:\/\/open\.spotify\.com\/track\/|https:\/\/music\.apple\.com\/[a-z][a-z]\/album\/[a-zA-Z\d%\(\)-]{1,100}\/|https://tidal\.com/browse/track/)(?:(?!\?).)*"#
+    case mentionsRegex = #"(?<=\@|@!)(\d+)(.*?)(?=\>)"#
+    case platformsRegex = #"(spotify|music\.apple|tidal)"#
+    case channelsRegex = ##"(?<=\#)(\d+)(.+?)(?=\>)"##
+    case lineRegex = #"\*.+\*|~~.+~~|`{1,3}.+`{1,3}|([^*~\s]+)+"#
+    case chatTextMentionsRegex = #"(?<=@)(?:(?!\ ).)*"#
+    case chatTextChannelsRegex = #"(?<=#)(?:(?!\ ).)*"#
+    case chatTextSlashCommandRegex = #"(?<=\/)(?:(?!\ ).)*"#
+    case chatTextEmojiRegex = #"(?<=:).*"#
+    case completedEmoteRegex = "(?<!<|<a):.+:"
 }
